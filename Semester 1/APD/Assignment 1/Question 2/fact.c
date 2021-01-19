b113eb9574fad627dbc30bd5ab9830dba742c0d5
Nehal Ahmad <nehal.ahmad@arnowa.com>
2021-01-19 14:11:19 +0530

    APD A1 Q2

--- Semester 1/APD/Assignment 1/Question 2/fact.c
+++ Semester 1/APD/Assignment 1/Question 2/fact.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
  
-int main() {
-  int number, iterator;
+long int factCalcRec(int number) {
+  if (number >= 1) {
+    return number*factCalcRec(number - 1);
+  } else {
+    return 1;
+  }
+}
+
+long int factCalcItr(int number) {
+  int iterator;
   long int fatorial = 1;
-  printf("Enter your number: ");
-  scanf("%d", &number);
   for (iterator = 1; iterator <= number; iterator++) {
     fatorial = iterator * fatorial;
   }
-  printf("Factorial is: %ld \n", fatorial);
+  return fatorial;
+}
+
+int main() {
+  int number, iterator;
+  printf("Enter your number: ");
+  scanf("%d", &number);
+  printf("Factorial using loop is: %ld \n", factCalcItr(number));
+  printf("Factorial using recursion is: %ld \n", factCalcRec(number));
 }
