36c7d2a7860c56e621edcd2d5d87f6d1b4631f48
Nehal Ahmad <mohammadamiramir773@gmail.com>
2021-01-20 12:37:00 +0530

    APD A1 Q3

--- Semester_1/APD/Assignment_1/Question_3/fib.c
+++ Semester_1/APD/Assignment_1/Question_3/fib.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 
 int recCounter = 0;
 
@@ -23,9 +24,20 @@ int fibItr(int number) {
 
 int main() { 
     int number;
+    clock_t t;
     printf("Enter your number: ");
     scanf("%d", &number);
-    printf("Fibonacci number using loop is: %d \n", fibItr(number)); 
-    printf("Fibonacci number using recursion is: %d \n", fibRec(number));
+
+    t = clock();
+    printf("\nFibonacci number using loop is: %d \n", fibItr(number)); 
+    t = clock() - t; 
+    double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds 
+    printf("Loop took %f seconds to complete \n", time_taken);
+
+    t = clock();
+    printf("\nFibonacci number using recursion is: %d \n", fibRec(number));
+    t = clock() - t; 
+    time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds 
+    printf("Recursion took %f seconds to complete \n", time_taken);
     printf("Number of function call is: %d \n", recCounter);
 } 
