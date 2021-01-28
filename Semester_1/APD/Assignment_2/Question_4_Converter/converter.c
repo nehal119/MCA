086cfcecffe7db0cdcb03f099872c62dd7ca28dc
Nehal Ahmad <mohammadamiramir773@gmail.com>
2021-01-28 22:35:20 +0530

    APD A2 Q4

--- Semester_1/APD/Assignment_2/Question_4_Converter/converter.c
+++ Semester_1/APD/Assignment_2/Question_4_Converter/converter.c
@@ -1,52 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void convert_to_x_base(int num, int base) {    
-    int rem;
-    // base condition
-    if (num == 0){
-        return;
+void conversion(int num, int base) {
+    int remainder = num % base;
+    if(num == 0) return;
+    conversion(num / base, base);
+    if(remainder < 10) {
+          printf("%d", remainder);
     }
     else {
-        rem = num % base; // get the rightmost digit        
-        convert_to_x_base(num/base, base);  // recursive call        
-        if(base == 16 && rem >= 10) {
-            printf("%c", rem+55);
-        }
-        else {
-            printf("%d", rem);
-        }
+          printf("%c", remainder - 10 + 'A' );
     }
-
 }
 
 void looperFunc() {
-    int num, choice, base;
-    printf("Select conversion type: \n\n");
-    printf("1. Decimal to binary. \n");
-    printf("2. Decimal to octal. \n");
-    printf("3. Decimal to hexadecimal. \n");              
-    printf("\nEnter your choice: ");
-    scanf("%d", &choice);
-    switch(choice) {
-        case 1:
-            base = 2;
-            break;
-        case 2:
-            base = 8;
-            break;              
-        case 3:
-            base = 16;
-            break;
-        default:
-            printf("Choice not available.\n\n");
-            return;
-    }
+    int num, choice;
     printf("Enter your number: ");
     scanf("%d", &num);
-    printf("Equivalent of %d in base %d is: ", num, base);
-    convert_to_x_base(num, base);
-    printf("\n\n");
+    printf("\nEquivalent of %d in base %d is: ", num, 2);
+    conversion(num, 2);
+    printf("\nEquivalent of %d in base %d is: ", num, 8);
+    conversion(num, 8);
+    printf("\nEquivalent of %d in base %d is: ", num, 16);
+    conversion(num, 16);
 }
 
 int main() {
