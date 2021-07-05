0b86bc86b6203aa9d644c4335e28e1a33db77b57
Nehal Ahmad <mohammadamiramir773@gmail.com>
2021-07-05 21:30:55 +0530

    pre, in, post

--- Extras/DS/Tree/tree.cpp
+++ Extras/DS/Tree/tree.cpp
@@ -13,6 +13,42 @@ struct Node {
   }
 };
 
+/*
+            1
+        2       3
+      4   5   6   7
+*/
+
+// Root -> Left Node -> Right Node
+void preorder(struct Node* root) {
+  if (root == NULL) {
+    return;
+  }
+  cout << root -> data << " ";
+  preorder(root -> left);
+  preorder(root -> right);
+}
+
+// Left Node  -> Root -> Right Node
+void inorder(struct Node* root) {
+  if (root == NULL) {
+    return;
+  }
+  inorder(root -> left);
+  cout << root -> data << " ";
+  inorder(root -> right);
+}
+
+// Left Node -> Right Node -> Root
+void postorder(struct Node* root){
+  if (root == NULL) {
+    return;
+  }
+  postorder(root -> left);
+  postorder(root -> right);
+  cout << root -> data << " ";
+}
+
 int main(){
   struct Node* root = new Node(1);
   root -> left = new Node(2);
@@ -21,4 +57,14 @@ int main(){
   root -> left -> right = new Node(5);
   root -> right -> left = new Node(6);
   root -> right -> right = new Node(7);
+
+  preorder(root);
+
+  cout << endl;
+  inorder(root);
+
+  cout << endl;
+  postorder(root);
+
+  return 0;
 }
