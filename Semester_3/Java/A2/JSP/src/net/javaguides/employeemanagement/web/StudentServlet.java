0bc2034723a21112da5ac7ceeb9c5876e80b70c7
Nehal Ahmad <mohammadamiramir773@gmail.com>
2021-12-03 12:23:58 +0530

    J2EE A2 (JSP + Database)

---
+++ Semester_3/Java/A2/JSP/src/net/javaguides/employeemanagement/web/StudentServlet.java
@@ -0,0 +1,53 @@
+package net.javaguides.studentmanagement.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.javaguides.studentmanagement.dao.StudentDao;
+import net.javaguides.studentmanagement.model.Student;
+
+/**
+ * @email Ramesh Fadatare
+ */
+
+@WebServlet("/register")
+public class StudentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private StudentDao studentDao;
+	
+	public void init() {
+		studentDao = new StudentDao();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String address = request.getParameter("address");
+		String contact = request.getParameter("contact");
+		
+		Student student = new Student();
+		student.setFirstName(firstName);
+		student.setLastName(lastName);
+		student.setUsername(username);
+		student.setPassword(password);
+		student.setContact(contact);
+		student.setAddress(address);
+		
+		try {
+			studentDao.registerStudent(student);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		response.sendRedirect("studentdetails.jsp");
+	}
+}
